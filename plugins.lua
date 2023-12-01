990159092e2f3301291f9cae754ae5df111b4a5d
Square-face <linus.michelsson@gmail.com>
2023-12-01 22:26:15 +0100

    added lazyvim plugin manager

---
+++ plugins.lua
@@ -0,0 +1,8 @@
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+})
