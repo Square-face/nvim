f7c62b1877153ff90ef516a3daf04851a4e016f4
Square-face <linus.michelsson@gmail.com>
2023-12-02 12:40:55 +0100

    added gruvbox colorscheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,10 +2,21 @@
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
-  "folke/which-key.nvim",
-  { "nvim/nvim-lspconfig", lazy=false },
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000, -- Load first
+        options = require "plugins.configs.gruvbox",
+
+        -- Loading colorscheme
+        config = function()
+              vim.cmd([[colorscheme gruvbox]])
+        end,
+    },
+
+    "folke/which-key.nvim",
+    { "nvim/nvim-lspconfig", lazy=false },
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    "folke/neodev.nvim",
 })
 
 
