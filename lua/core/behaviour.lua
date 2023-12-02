ffd72e6194167c410e7bf0dbc30fba6e5d89a7fa
Square-face <linus.michelsson@gmail.com>
2023-12-02 12:20:41 +0100

    fixed tabs

---
+++ lua/core/behaviour.lua
@@ -0,0 +1,9 @@
+-- Set tabs to be represented as spaces
+vim.o.tabstop = 4 -- Number of spaces tabs count for
+vim.o.shiftwidth = 4 -- Number of spaces used for autoindent
+vim.o.expandtab = true -- Convert tabs to spaces
+
+-- Additional settings for indentation
+vim.o.autoindent = true -- Enable autoindentation based on previous line
+vim.o.smartindent = true -- Smart autoindentation
+
