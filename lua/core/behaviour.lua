cb763306a3ad5c1728ff963f138e00916b4a5223
Square-face <linus.michelsson@gmail.com>
2023-12-02 13:22:00 +0100

    added line numbers

--- lua/core/behaviour.lua
+++ lua/core/behaviour.lua
@@ -1,9 +1,14 @@
 -- Set tabs to be represented as spaces
-vim.o.tabstop = 4 -- Number of spaces tabs count for
-vim.o.shiftwidth = 4 -- Number of spaces used for autoindent
-vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.tabstop = 4               -- Number of spaces tabs count for
+vim.o.shiftwidth = 4            -- Number of spaces used for autoindent
+vim.o.expandtab = true          -- Convert tabs to spaces
 
 -- Additional settings for indentation
-vim.o.autoindent = true -- Enable autoindentation based on previous line
-vim.o.smartindent = true -- Smart autoindentation
+vim.o.autoindent = true         -- Enable autoindentation based on previous line
+vim.o.smartindent = true        -- Smart autoindentation
 
+vim.o.number = true             -- enable line numbers
+vim.o.relativenumber = true     -- enable relative line numbers
+
+vim.o.signcolumn = 'no'        -- Always show the signcolumn, to avoid shifting numbers when using Vim's default 'auto' setting
+vim.o.scrolloff = 3             -- Number of lines to keep above and below the cursor
