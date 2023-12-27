30181bb3625652a5105cd08112c9056f2b248793
Square-Face <sq8.square.face@gmail.com>
2023-12-27 19:19:40 +0100

    lualine overhaul

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -1,18 +1,67 @@
+
 local options = {
     options = {
         theme = 'tokyonight',
         icons_enabled = true,
     },
+
     sections = {
         lualine_a = { 'mode' },
+
         lualine_b = { 'branch' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
+
+        lualine_c = {
+            {
+                'filename',
+
+                file_status = true,
+                path = 1,
+            },
+        },
+
+
+
+
+        lualine_x = {
+            {
+                'diagnostics',
+
+                -- Table of diagnostic sources, available sources are:
+                --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+                -- or a function that returns a table as such:
+                --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+                sources = { 'nvim_lsp' },
+
+                -- Displays diagnostics for the defined severity types
+                sections = { 'error', 'warn', 'info', 'hint' },
+
+                diagnostics_color = {
+                    -- Same values as the general color option can be used here.
+                    error = 'DiagnosticError', -- Changes diagnostics' error color.
+                    warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+                    info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+                    hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+                },
+
+                symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
+                colored = true,           -- Displays diagnostics status in color if set to true.
+                update_in_insert = true, -- Update diagnostics in insert mode.
+                always_visible = true,   -- Show diagnostics even if there are none.
+            },
+            {
+                'filetype',
+
+                icon_only = false,
+                newfile_status = true,
+            },
+        },
+
+        lualine_y = { 'encoding' },
+
+        lualine_z = { 'location', 'progress' },
     },
-    tabline = {},
-    extensions = {}
+
+    extensions = { 'toggleterm' }
 }
 
 
