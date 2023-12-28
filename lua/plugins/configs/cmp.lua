1908554c139f94f557ce3c64fb6f5a0ac093bb74
Square-Face <sq8.square.face@gmail.com>
2023-12-28 03:18:06 +0100

    ceaned up cmp configs

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -48,17 +48,7 @@ M.config = function()
             },
         },
 
-        enabled = function()
-            -- disable completion in comments
-            local context = require 'cmp.config.context'
-            -- keep command mode completion enabled when cursor is in a comment
-            --
-            if vim.api.nvim_get_mode().mode == 'c' then
-                return true
-            end
-
-            return true --not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
-        end,
+        enabled = true,
 
         mapping = {
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
