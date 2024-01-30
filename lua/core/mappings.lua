e3a473debac339110407b4c6a6715ff6745cbb41
Square-face <linus.michelsson@gmail.com>
2024-01-30 20:01:58 +0100

    fixed some colorscheme settings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -11,12 +11,6 @@ M.n = {
     ['<A-j>'] = { ':m+1 <CR>', 'Move line down' },
     ['<A-k>'] = { ':m-2 <CR>', 'Move line up' },
 
-    -- Move from buffer to buffer
-    ['<C-h>'] = { '<C-w>h', 'Move to left buffer' },
-    ['<C-j>'] = { '<C-w>j', 'Move to down buffer' },
-    ['<C-k>'] = { '<C-w>k', 'Move to up buffer' },
-    ['<C-l>'] = { '<C-w>l', 'Move to right buffer' },
-
     -- File
     ['<C-s>'] = { ':w<CR>', 'Save current file'},
     ['<Esc>'] = { ':noh<CR>', 'Remove search highlight'},
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,6 +17,17 @@ require('lazy').setup({
 
     }, 'plugins.configs.tokyonight'),
 
+    {
+        'alexghergh/nvim-tmux-navigation',
+
+        keys = {
+            { '<C-l>', function() require('nvim-tmux-navigation').NvimTmuxNavigateRight() end },
+            { '<C-h>', function() require('nvim-tmux-navigation').NvimTmuxNavigateLeft() end },
+            { '<C-k>', function() require('nvim-tmux-navigation').NvimTmuxNavigateUp() end },
+            { '<C-j>', function() require('nvim-tmux-navigation').NvimTmuxNavigateDown() end },
+        }
+    },
+
 
     extended({ -- Statusline
 
@@ -25,7 +36,7 @@ require('lazy').setup({
 
     }, 'plugins.configs.lualine'),
 
-    extended({
+    extended({ -- Tabs
         'romgrk/barbar.nvim',
         lazy = false,
 
