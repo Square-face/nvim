f4d6f04554e5004b1008f42dddac2dd70f55e18f
Square-face <linus.michelsson@gmail.com>
2024-02-26 10:00:46 +0000

    fixed code actions and markdown preview

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -63,14 +63,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end, opts)
         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<leader>ca', function()
-            -- check if file type is rust
-            if vim.bo.filetype == 'rust' then
-                vim.cmd.RustLsp { 'hover', 'actions' }
-            else
-                vim.lsp.buf.code_action()
-            end
-        end, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', '<leader>f', function()
             vim.lsp.buf.format { async = true }
