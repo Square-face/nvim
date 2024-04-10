c52583956db40c81c707343d628eb3c273618a28
Square-face <linus.michelsson@gmail.com>
2024-04-10 11:21:36 +0000

    Made treesitter actually work

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -4,10 +4,10 @@ local M = {}
 
 M.opts = {
     -- A list of parser names, or 'all' (the five listed parsers should always be installed)
-    ensure_installed = { 'all' },
+    ensure_installed = { 'svelte', 'typescript', 'javascript', 'rust', 'sql', 'python', 'lua', 'bash', 'toml', 'yaml', 'json', 'markdown' },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+    sync_install = true,
 
     -- Automatically install missing parsers when entering buffer
     auto_install = true,
@@ -16,5 +16,11 @@ M.opts = {
         enable = true,
     },
 }
+
+M.build = ":TSUpdate"
+
+M.config = function()
+    require'nvim-treesitter.configs'.setup(M.opts)
+end
 
 return M
