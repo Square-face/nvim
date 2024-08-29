f76683f9e7c5cd7dc560554061a24ecb81fabfe4
Square-face <sq8.square.face@gmail.com>
2024-08-29 14:51:07 +0200

    more telescope keybinds

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -56,14 +56,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<leader>ca', function ()
@@ -73,7 +65,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
                 vim.lsp.buf.code_action()
         end
         end, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', '<leader>f', function()
             vim.lsp.buf.format { async = true }
         end, opts)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -179,11 +179,14 @@ require('lazy').setup({
         },
         cmd = { 'Telescope' },
         keys = {
-            { 'ff', function() require 'telescope.builtin'.find_files() end, desc = 'Find files' },
-            { 'fg', function() require 'telescope.builtin'.live_grep() end,  desc = 'Find files with grep' },
-            { 'fb', function() require 'telescope.builtin'.buffers() end,    desc = 'Find buffers' },
-            { 'fh', function() require 'telescope.builtin'.help_tags() end,  desc = 'Find help' },
-            { 'ft', function() require 'telescope.builtin'.treesitter() end, desc = 'Find symbols (treesitter)' },
+            { 'ff', function() require 'telescope.builtin'.find_files() end,            desc = 'Find files' },
+            { 'fg', function() require 'telescope.builtin'.live_grep() end,             desc = 'Find files with grep' },
+            { 'fb', function() require 'telescope.builtin'.buffers() end,               desc = 'Find buffers' },
+            { 'fh', function() require 'telescope.builtin'.help_tags() end,             desc = 'Find help' },
+            { 'fs', function() require 'telescope.builtin'.treesitter() end,            desc = 'Find symbols (treesitter)' },
+            { 'fr', function() require 'telescope.builtin'.lsp_refrences() end,         desc = 'Find refrences (LSP)' },
+            { 'fd', function() require 'telescope.builtin'.lsp_definitions() end,       desc = 'Find definitions (LSP)' },
+            { 'fi', function() require 'telescope.builtin'.lsp_implementations() end,   desc = 'Find implementations (LSP)' },
         }
     },
 
