43daa6b144aa59b07121aeb0d7f7c0d98dc0ea5e
Square-face <sq8.square.face@gmail.com>
2024-08-29 14:00:46 +0200

    Rust code action now utelizes Rustaceanvim's colapsing action browser

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -65,7 +65,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end, opts)
         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', function ()
+            if vim.bo.filetype == "rust" then
+                vim.cmd.RustLsp('codeAction')
+            else
+                vim.lsp.buf.code_action()
+        end
+        end, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', '<leader>f', function()
             vim.lsp.buf.format { async = true }
