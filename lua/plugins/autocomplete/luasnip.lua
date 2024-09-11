4ecfeac6b6b39fe18ff7796395ed17249d164cf4
Square-face <sq8.square.face@gmail.com>
2024-09-11 18:36:03 +0200

    feat: added rafamadriz/friendly-snippets

--- lua/plugins/autocomplete/luasnip.lua
+++ lua/plugins/autocomplete/luasnip.lua
@@ -1,7 +1,12 @@
-local Plugin = {'L3MON4D3/LuaSnip'}
+local Plugin = { 'L3MON4D3/LuaSnip' }
 Plugin.lazy = true
+
 Plugin.build = vim.g.isnix and "nix-shell -p gnumake --run 'make install_jsregexp'" or "make install_jsregexp"
-Plugin.dependencies = 'rafamadriz/friendly-snippets'
+
 Plugin.opts = { history = true, updateevents = 'TextChanged,TextChangedI' }
+
+Plugin.dependencies = {
+    'rafamadriz/friendly-snippets',
+}
 
 return Plugin
