97d028686ccbe0889bdfe390362162bb412cff47
Square-face <sq8.square.face@gmail.com>
2024-09-11 18:36:38 +0200

    chore: formatting

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -1,7 +1,7 @@
 return {
-    { 'j-hui/fidget.nvim',                   event = "VeryLazy",         opts = { integration = { ["nvim-tree"] = { enable = false } } } }, -- Very unclear what this integration does except make the startup take 30 extra ms
+    { 'j-hui/fidget.nvim',                   event = "VeryLazy",           opts = { integration = { ["nvim-tree"] = { enable = false } } } }, -- Very unclear what this integration does except make the startup take 30 extra ms
     { 'folke/which-key.nvim',                event = 'VeryLazy' },
-    { "karb94/neoscroll.nvim",               opts = {},                  keys = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb', } },
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl",               opts = {} },
+    { "karb94/neoscroll.nvim",               opts = {},                    keys = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb', } },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl",                 opts = {} },
     { "luukvbaal/statuscol.nvim",            opts = { relculright = true } },
 }
