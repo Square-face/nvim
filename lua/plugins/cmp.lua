abe8030f6eee3f71260ced5ac51f87256dd81207
Square-face <sq8.square.face@gmail.com>
2024-10-05 19:38:36 +0200

    added: luasnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,21 +4,43 @@ plug.event = "InsertEnter"
 plug.dependencies = {
     'neovim/nvim-lspconfig',
     'hrsh7th/cmp-nvim-lsp',
+    'L3MON4D3/LuaSnip',
 }
 
-local function super_tab(fallback)
+local function cr(fallback)
     local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+
     if cmp.visible() then
-        cmp.select_next_item()
+        cmp.confirm({ select = true })
+    elseif luasnip.expandable() then
+        luasnip.expand()
     else
         fallback()
     end
 end
 
-local function shift_super_tab(fallback)
+local function tab(fallback)
     local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+
+    if cmp.visible() then
+        cmp.select_next_item()
+    elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+    else
+        fallback()
+    end
+end
+
+local function shift_tab(fallback)
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+
     if cmp.visible() then
         cmp.select_prev_item()
+    elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
     else
         fallback()
     end
@@ -29,17 +51,22 @@ plug.opts = function()
 
     return {
         mapping = {
-            ['<C-d>']   = cmp.mapping.scroll_docs(4),
-            ['<C-u>']   = cmp.mapping.scroll_docs(-4),
+            ['<C-d>']     = cmp.mapping.scroll_docs(4),
+            ['<C-u>']     = cmp.mapping.scroll_docs(-4),
 
-            ['<Up>']    = cmp.mapping.select_prev_item(),
-            ['<Down>']  = cmp.mapping.select_next_item(),
+            ['<Up>']      = cmp.mapping.select_prev_item(),
+            ['<Down>']    = cmp.mapping.select_next_item(),
 
-            ['<CR>']    = cmp.mapping.confirm({ select = true }),
-            ['<Esc>']   = cmp.mapping.abort(),
+            ['<CR>']      = cmp.mapping(cr, { 'i', 's' }),
+            ['<C-Space>'] = cmp.mapping.abort(),
 
-            ['<Tab>']   = cmp.mapping(super_tab, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(shift_super_tab, { 'i', 's' }),
+            ['<Tab>']     = cmp.mapping(tab, { 'i', 's' }),
+            ['<S-Tab>']   = cmp.mapping(shift_tab, { 'i', 's' }),
+        },
+        snippet = {
+            expand = function(args)
+                require 'luasnip'.lsp_expand(args.body)
+            end
         },
         sources = cmp.config.sources({
             { name = 'nvim_lsp' }
---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,6 @@
+local plug = {'L3MON4D3/LuaSnip'}
+plug.build = "make install_jsregexp"
+plug.dependencies = { 'rafamadriz/friendly-snippets' }
+plug.lazy = true
+
+return plug
