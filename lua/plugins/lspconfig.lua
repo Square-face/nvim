44f7b3ac1ac71e2672f7b44ec5d3b86dee5fe697
Square-face <sq8.square.face@gmail.com>
2024-10-06 01:35:31 +0200

    keybind(lsp): <leader>j/k -> <C-j/k> for jumping between diagnostics

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,8 +10,8 @@ local keys = {
     { '<leader>rn', vim.lsp.buf.rename,       silent = true, noremap = true, desc = "Rename symbol." },
     { '<leader>ca', vim.lsp.buf.code_action,  silent = true, noremap = true, desc = "Use code acion." },
     { '<leader>f',  vim.lsp.buf.format,       silent = true, noremap = true, desc = "Format code." },
-    { '<leader>j',  vim.diagnostic.goto_next, silent = true, noremap = true, desc = "Jump to next diagnostic." },
-    { '<leader>k',  vim.diagnostic.goto_prev, silent = true, noremap = true, desc = "Jump to previous diagnostic." },
+    { '<C-j>',  vim.diagnostic.goto_next, silent = true, noremap = true, desc = "Jump to next diagnostic." },
+    { '<C-k>',  vim.diagnostic.goto_prev, silent = true, noremap = true, desc = "Jump to previous diagnostic." },
 }
 
 
