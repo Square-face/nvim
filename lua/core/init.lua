b190bed639634168cbf5810ef3af389309dcd0f2
Square-face <sq8.square.face@gmail.com>
2024-10-05 20:26:02 +0200

    color: made current line number distictive

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -8,3 +8,5 @@ vim.o.shiftwidth = 4
 -- line numbers
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.signcolumn = 'number'
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,11 +7,12 @@ local plug = {
 plug.opts = {
     plugins = {
         telescope = true
-    }
+    },
 }
 
 plug.init = function()
     vim.cmd.colorscheme('horizon')
+    vim.api.nvim_set_hl(0, 'CursorLineNr', {fg='#ffffff'})
 end
 
 return plug
