a94ba09be79a8698105e5eae9d7f564954a69301
Square-face <sq8.square.face@gmail.com>
2024-10-08 14:45:14 +0200

    added: sneak

---
+++ lua/plugins/sneak.lua
@@ -0,0 +1,12 @@
+local plug = {'justinmk/vim-sneak'}
+
+plug.keys = {
+    {'s', '<Plug>Sneak_s'},
+    {'S', '<Plug>Sneak_S'},
+    {'f', '<Plug>Sneak_f'},
+    {'F', '<Plug>Sneak_F'},
+    {'t', '<Plug>Sneak_t'},
+    {'T', '<Plug>Sneak_T'},
+}
+
+return plug
