fc73e3013690974440b88c5c1a25f8f9d532003c
SQ8 <linus@sq8.dev>
2025-02-08 16:51:11 +0100

    changed surround keybind

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -8,7 +8,7 @@ plug.keys = {
     { 'ys',  mode = 'n', desc = "Create surround" },
     { 'yS',  mode = 'n', desc = "Create surround (newline)" },
     { 'yss', mode = 'n', desc = "Create surround (entire line)" },
-    { 'ySS', mode = 'n', desc = "Create surround (for entire line) (newline)" },
+    { 'ysS', mode = 'n', desc = "Create surround (for entire line) (newline)" },
 }
 
 plug.opts = {}
