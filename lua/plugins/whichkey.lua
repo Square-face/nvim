a24599f3f7291f242d85068405d1c056c2d71317
SQ8 <linus@sq8.dev>
2025-02-23 01:48:16 +0100

    added keybind to auto generate random floats

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,6 +2,21 @@ local plug = { 'folke/which-key.nvim', name = 'which-key' }
 
 plug.event = 'VeryLazy'
 
+local function random_float()
+    local rand_float = string.format("%.2f", math.random() * 100)
+
+    -- Check if we are in visual mode
+    local mode = vim.fn.mode()
+    if mode == "v" or mode == "V" or mode == "\22" then     -- "\22" is CTRL-V (block mode)
+        -- Replace visual selection
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "x", false)
+        vim.cmd("normal! gv\"_d", false)
+    end
+
+    -- Insert the random float
+    vim.api.nvim_put({ rand_float }, "", true, true)
+end
+
 plug.opts = {
     spec = {
         { '<ESC>', '<cmd>nohlsearch<CR>',                                        mode = 'n', silent = true, noremap = true, desc = 'Hide search results', },
@@ -17,6 +32,10 @@ plug.opts = {
             { '<A-k>', function() vim.cmd("m'<-" .. 1 + vim.v.count1) end, noremap = true, desc = 'Move line up' },
             { '<',     '<gv',                                              noremap = true, desc = 'Unindent line' },
             { '>',     '>gv',                                              noremap = true, desc = 'Unindent line' },
+        },
+        {
+            mode = { 'n', 'v' },
+            { '<leader>rf', random_float, noremap = true, desc = 'Generate a random float value between 0 and 100' },
         },
         {
             mode = { 't' },
