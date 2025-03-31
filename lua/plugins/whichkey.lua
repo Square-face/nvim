24ca7184aa6c2588bf627b407f1c90ef0362c554
SQ8 <linus@sq8.dev>
2025-03-31 16:49:38 +0200

    moved random float

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,21 +2,6 @@ local plug = { 'folke/which-key.nvim', name = 'which-key' }
 
 plug.event = 'VeryLazy'
 
-local function random_float()
-    local rand_float = string.format("%.2f", math.random() * 100)
-
-    -- Check if we are in visual mode
-    local mode = vim.fn.mode()
-    if mode == "v" or mode == "V" or mode == "\22" then     -- "\22" is CTRL-V (block mode)
-        -- Replace visual selection
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "x", false)
-        vim.cmd("normal! gv\"_d", false)
-    end
-
-    -- Insert the random float
-    vim.api.nvim_put({ rand_float }, "", true, true)
-end
-
 plug.opts = {
     spec = {
         { '<ESC>', '<cmd>nohlsearch<CR>',                                        mode = 'n', silent = true, noremap = true, desc = 'Hide search results', },
@@ -35,7 +20,7 @@ plug.opts = {
         },
         {
             mode = { 'n', 'v' },
-            { '<leader>rf', random_float, noremap = true, desc = 'Generate a random float value between 0 and 100' },
+            { '<leader>rf', require('core.random_float').insert_random_float, noremap = true, desc = 'Generate a random float value between 0 and 100' },
         },
         {
             mode = { 't' },
