7b9f74174350c1d5160f3938b8c816556c9a34a6
SQ8 <linus@sq8.dev>
2025-05-15 15:24:31 +0200

    feat: added plugin for Cargo.toml files

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,16 @@
+local crates = { 'saecki/crates.nvim', tag = 'stable', name = 'crates' }
+crates.event = 'BufRead Cargo.toml'
+crates.opts = {
+    lsp = {
+        enabled = true,
+        on_attach = function(_, _) end,
+        actions = true,
+        completion = true,
+        hover = true,
+    },
+}
+
+local rustacean = { 'mrcjkb/rustaceanvim', version = '5.2.1' }
+rustacean.ft = 'rust'
+
+return { rustacean, crates }
--- lua/plugins/rustaceanvim.lua
+++
@@ -1,4 +0,0 @@
-local plug = {'mrcjkb/rustaceanvim', version = '5.2.1', lazy=false}
-plug.ft = 'rust'
-
-return plug
