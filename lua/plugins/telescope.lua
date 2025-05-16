0b88ed79860b1c0da4bf64abf565542b3c63b1c3
SQ8 <linus@sq8.dev>
2025-05-16 10:53:37 +0200

    chore: slight cleanup in telescope plugin file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,19 @@
-local plug = { 'nvim-telescope/telescope.nvim' }
-plug.dependecies = { 'nvim-lua/plenary.nvim' }
+local ts = { 'nvim-telescope/telescope.nvim' }
 
-plug.keys = {
-    { 'ff', '<cmd>Telescope find_files<cr>',         silent = true, noremap = true, desc = "Find files" },
-    { 'fg', '<cmd>Telescope live_grep<cr>',          silent = true, noremap = true, desc = "Grep files" },
-    { 'fb', '<cmd>Telescope buffers<cr>',            silent = true, noremap = true, desc = "Find buffers" },
-    { 'fh', '<cmd>Telescope help_tags<cr>',          silent = true, noremap = true, desc = "Find help" },
-    { 'fr', '<cmd>Telescope lsp_references<cr>',     silent = true, noremap = true, desc = "Lsp Refrences" },
-    { 'fo', '<cmd>Telescope lsp_incoming_calls<cr>', silent = true, noremap = true, desc = "Lsp outgoing calls" },
-    { 'fi', '<cmd>Telescope lsp_outgoing_calls<cr>', silent = true, noremap = true, desc = "Lsp incomming calls" },
+ts.dependencies = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' }
+
+ts.keys = {
+    { 'Ff', '<cmd>Telescope find_files<cr>',         silent = true, noremap = true, desc = "Find files" },
+    { 'Fg', '<cmd>Telescope live_grep<cr>',          silent = true, noremap = true, desc = "Grep files" },
+    { 'Fb', '<cmd>Telescope buffers<cr>',            silent = true, noremap = true, desc = "Find buffers" },
+    { 'Fh', '<cmd>Telescope help_tags<cr>',          silent = true, noremap = true, desc = "Find help" },
+    { 'Fr', '<cmd>Telescope lsp_references<cr>',     silent = true, noremap = true, desc = "Lsp Refrences" },
+    { 'Fo', '<cmd>Telescope lsp_incoming_calls<cr>', silent = true, noremap = true, desc = "Lsp outgoing calls" },
+    { 'Fi', '<cmd>Telescope lsp_outgoing_calls<cr>', silent = true, noremap = true, desc = "Lsp incomming calls" },
 }
 
-plug.cmd = { 'Telescope' }
+ts.opts = { }
 
-return plug
+ts.cmd = { 'Telescope' }
+
+return ts
