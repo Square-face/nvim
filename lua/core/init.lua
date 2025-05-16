34b4f2540dd29a73c723c477391d11c96bfb883e
SQ8 <linus@sq8.dev>
2025-05-16 20:14:58 +0200

    feat: more folding fixes

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -20,6 +20,10 @@ vim.o.signcolumn = 'number'
 vim.o.clipboard = 'unnamedplus'
 
 -- Folds
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
 vim.o.foldcolumn = '1'
 vim.o.foldenable = true
 vim.o.fillchars = 'foldopen:▾,foldsep:│,foldclose:▸'
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.lsp.foldexpr()"
