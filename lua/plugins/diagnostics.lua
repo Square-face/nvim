3dc0b5fb1eaf300f1d1c1b455eb7edcb0eb0e3a6
SQ8 <linus@sq8.dev>
2025-05-16 21:53:47 +0200

    fix: diagnostics while in insert more

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -6,9 +6,11 @@ diagnostics.opts = {
     preset = "modern",
 
     options = {
+        throttle = 0,
+        enable_on_insert = true,
         use_icons_from_diagnostic = true,
         multilines = { enabled = true },
-        break_line = { enabled = true }
+        break_line = { enabled = true },
     }
 }
 
