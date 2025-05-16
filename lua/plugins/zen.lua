1d7b521047e26ae7e73539097e6f39f8a6bc0bed
SQ8 <linus@sq8.dev>
2025-05-16 10:46:41 +0200

    feat: added zen mode

---
+++ lua/plugins/zen.lua
@@ -0,0 +1,43 @@
+local twilight = { "folke/twilight.nvim", name = 'twilight' }
+
+twilight.opts = {
+    dimming = {
+        alpha = 0.5,
+        color = { "Normal", "#ffffff" },
+        term_bg = "#000000",
+        inactive = false,
+    },
+    context = 15,
+    expand = {
+        "function",
+        "method",
+        "table",
+        "if_statement",
+    },
+}
+
+local zen = { "folke/zen-mode.nvim", name = "zen-mode" }
+zen.dependencies = { twilight }
+
+zen.opts = {
+    window = {
+        width = 200,
+        options = {
+            number = false,
+            cursorcolumn = false,
+            foldcolumn = "0",
+            list = false
+        }
+    },
+    plugins = {
+        gitsigns = { enabled = true },
+        kitty = { enabled = true },
+        tmux = { enabled = true }
+    }
+}
+
+zen.keys = {
+    { '<leader>z', function() require('zen-mode').toggle() end, desc = "Toggle Zen mode" }
+}
+
+return zen
