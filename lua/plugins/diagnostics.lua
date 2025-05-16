1d6a17d9f02148e40afda98ddca9601b978a8de3
SQ8 <linus@sq8.dev>
2025-05-16 21:35:28 +0200

    feat: diagnostics

---
+++ lua/plugins/diagnostics.lua
@@ -0,0 +1,15 @@
+local diagnostics = { "rachartier/tiny-inline-diagnostic.nvim", name = "inline-diagnostic" }
+diagnostics.event = "LspAttach"
+diagnostics.priority = 1000
+
+diagnostics.opts = {
+    preset = "modern",
+
+    options = {
+        use_icons_from_diagnostic = true,
+        multilines = { enabled = true },
+        break_line = { enabled = true }
+    }
+}
+
+return diagnostics
